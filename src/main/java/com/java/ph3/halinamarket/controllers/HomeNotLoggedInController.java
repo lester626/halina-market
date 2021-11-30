820e19d15992d4f4b89f50221a0402166a138191
iamlma <ancheta.lailamarie@gmail.com>
2021-11-30 14:10:05 +0800

    user not logged in

--- src/main/java/com/java/ph3/halinamarket/controllers/HomeNotLoggedInController.java
+++ src/main/java/com/java/ph3/halinamarket/controllers/HomeNotLoggedInController.java
@@ -3,37 +3,25 @@ package com.java.ph3.halinamarket.controllers;
 import com.java.ph3.halinamarket.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeController {
+public class HomeNotLoggedInController {
 
     @Autowired
     CategoryRepository categoryRepository;
 
     @GetMapping("/home")
-    public String home(Model model, ModelMap modelMap) {
-        model.addAttribute("welcomeHome", "Your favorite neighborhood store");
+    public String home(ModelMap modelMap) {
         modelMap.addAttribute("categories", categoryRepository.findAll());
         return "homeNotLoggedIn";
     }
 
     @RequestMapping(value = "/signup")
     public String signUp() {
-        return "signup";
+        return "signUp";
     }
 
-    @GetMapping("/admin")
-    public String admin() {
-        return "admin";
-    }
-
-    @GetMapping("/categories")
-    public String categories(Model model) {
-
-        return "categories";
-    }
 }
